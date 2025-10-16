814e3844a72d8d19d10ffea0835b14895af486bf
Albert Dugba <albert.dugba@deciphex.com>
2025-10-16 10:50:40 +0100

    testing again

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(2)
+  const [count, setCount] = useState(4);
 
   return (
     <>
+      <h2>Hello zapier test</h2>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
